9cdf1b763cf7f8dd77dcfe4402b96c973a12d873
Erlan <erlan.u@1fit.app>
2022-06-09 01:07:52 +0600

    deploy initt

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/keloynaa/'
-        : '/',
-  transpileDependencies: true
-})
+        ? '/my-project/'
+        : '/'
+}
